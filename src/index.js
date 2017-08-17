3e89fafc67566a0aac70d3d938592f843371c4b6
sotongc <c_zero@live.cn>
2017-08-17 17:46:31 +0800

    add method to recognize pattern

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@
 //*[@id="1"]/div[2]/em[6]
 //*[@id="sixapart-standard"]/head
 //*[@id="sixapart-standard"]/head/title
+//*[@id="cnblogs_post_body"]/p[3]
+//*[@id="cnblogs_post_body"]/p[1]
+//*[@id="cnblogs_post_body"]/div[1]/a
 
+var isUnique=(pattern)=>{
+	return /(html|body|head|title|\*\[\@id\=\"[\w\d]+\"\])/.test(pattern);
+};
 
 var decopose_xpath=(xpath)=>{
 	let chain=[];
@@ -21,12 +27,68 @@ var decopose_xpath=(xpath)=>{
 };
 
 var xpath_chain_reduce=(chain)=>{
-	let unique=/(html|body|head|title|\*\[\@id\=\"[\w\d]+\"\])/;
 	let indexof_first_unique=0;
 
 	for(let i=0;i<chain.length;i++){
-		unique.test(chain[i])&&(indexof_first_unique=i);
+		isUnique(chain[i]) &&(indexof_first_unique=i);
 	}
 
 	return chain.slice(indexof_first_unique);
+};
+
+var judge_type=(regexp,pattern)=>{
+	let type=-1;
+	Object.keys(regexp).slice(1).some((key,i)=>{
+		type=regexp[key].test(pattern)?i:-1;
+		return !type==-1;
+	});
+	return type;
+};
+
+var selector={
+	id:(xpath)=>{
+		return xpath.replace(/(\/|\/\/)?(\*\[\@id\=\"([\w\d-]+)\"\])/,($1,$2,$3,$4)=>{
+			return ($2==="\u005c"?"\u003e":($2==="\u005c\u005c"?"\u0020":""))+"#"+$4;
+		});
+	},
+	class:(xpath)=>{
+		return xpath.replace(/(\/|\/\/)?(\*\[\@class\=\"([\w\d-]+)\"\])/,($1,$2,$3,$4)=>{
+			return ($2==="\u005c"?"\u003e":($2==="\u005c\u005c"?"\u0020":""))+"\u002e"+$4;
+		});
+	},
+	tag:(xpath)=>{
+		let i=0;
+		return `.querySelectorAll(${xpath.replace(/(\/|\/\/)?([a-z]+)(\[\d+\])?/,($1,$2,$3,$4)=>{
+			i=$4-1;
+			return ($2==="\u005c"?"\u003e":($2==="\u005c\u005c"?"\u0020":""))+$3
+		})})[${i}]`;
+	},
+	attr:(xpath)=>{
+		return xpath.replace(/(\/|\/\/)?(\*\[\@(?!id|class)([\w-]+)\=\"([\w\d-]+)\"\])/,($1,$2,$3,$4,$5)=>{
+			return ($2==="\u005c"?"\u003e":($2==="\u005c\u005c"?"\u0020":""))+"["+$4+"='"+$5+"']";
+		});
+	}
+};
+
+var handler=[selector.id,selector.class,selector.tag,selector.attr];
+
+
+
+var compiler=(chain)=>{
+	let regexp={
+		is_first:/^\/{1,2}/,
+		is_id:/\/+\*\[\@id\=\"[\w\d-]+\"\]/,
+		is_class:/\/+\*\[\@class\=\"[\w\d_-]+\"\]/,
+		is_tag:/\/+[a-z]+(\[\d+\])?/,
+		is_attr:/\/+(\*|[a-z]+)\[\@(?!id|class)[\w-]+\=\"[\w\d_-]+\"\]/
+	};
+
+	return chain.map((xpath,i)=>{
+		//remove the directory for the root node
+		if(!i)
+			xpath=xpath.replace(regexp.is_first,"");
+
+		//compile the xpath to the selector
+		
+	});
 };
